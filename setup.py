a9dafd4e00670eed05bf5392e3bb3f4dce7a01b0
Robinson04 <robinson.labourdette@gmail.com>
2020-09-16 17:23:46 +0200

    Separated tests and library

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="StructNoSQL",
+    version="0.2",
+    packages=["structnosql", "structnosql.dynamodb", "structnosql.utils"],
+    include_package_data=True,
+    install_requires=["pydantic", "boto3"],
+    url="https://github.com/Robinson04/StructNoSQL",
+    license="MIT",
+    author="Inoft",
+    author_email="robinson@inoft.com",
+    description="Structured document based NoSQL client for DynamoDB with automatic data validation and advanced database queries functions.",
+)
+

7022a4568e11784de152d72df9279494da194586
Robinson Labourdette <robinson@inoft.com>
2021-07-21 21:10:25 +0200

    v2.6.0 - ExternalDynamoDBApiTable's replace the InoftVocalEngineTable's
    - Re-implement the query_field and query_multiple_fields operation's in the ExternalDynamoDBApiTable's's that were desactivated for the v2.5.0
    - Migrated InoftVocalEngine table's to ExternalDynamoDBApiTable's
    - Added an external_dynamodb_api server_middleware
    - Added a small flask backend to run during unittests in order to create an api that allow the usage of the external_dynamodb_api server_middleware
    - Major refactor and renaming of folders

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="StructNoSQL",
-    version="2.5.0",
+    version="2.6.0",
     packages=find_packages(),
     include_package_data=True,
     install_requires=["pydantic", "boto3"],

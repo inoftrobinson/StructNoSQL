1d06437e1fa0a092f498aa5cbdb966e949372293
Robinson Labourdette <robinson@inoft.com>
2022-11-23 18:22:08 +0100

    Added package_nightly_release.py and serverlesspack.config.yaml files

---
+++ package_nightly_release.py
@@ -0,0 +1,8 @@
+import os
+
+try:
+    from serverlesspack import package_api
+    config_filepath: str = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'serverlesspack.config.yaml')
+    package_api(target_os='linux', config_filepath=config_filepath, verbose=False)
+except ModuleNotFoundError as e:
+    print("serverlesspack library not found. Install it by following the instructions at : https://github.com/Robinson04/serverlesspack")
